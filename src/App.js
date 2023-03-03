e47357dbbbabe1fabb84d32e3bcc6a5b01b617e8
Jetsadakorn <my1471@hotmail.com>
2023-03-03 17:02:07 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,33 +1,55 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import PhotoComponent from './component/PhotoComponent';
-
+import PhotoComponent from "./components/PhotoComponent";
+import {useEffect, useState}  from "react"
+import './App.css'
 
 function App() {
-  const apiKey = `xk0rQCrVnf5KGcOeetytouJzA85K_iO3rqZf7ExAW_k`
-  const apiURL = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=1`
-
+  const apiKey = `zp0BGJGUJtBhrTEzacC0UXzyjQ5M2efitpF7Vzo5xBE`
   const [photos,setPhotos] = useState([])
+  const [page,setPage] = useState(1) // 2,3,4,5,6
+  const [isLoading,setIsLoading] = useState(false)
 
   const fetchImage=async()=>{
-    const response = await fetch(apiURL)
-    const data = response.json()
-    setPhotos(data)
+      setIsLoading(true)
+      try {
+        const apiUrl = `https://api.unsplash.com/photos/?client_id=${apiKey}&page=${page}`
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        setPhotos((oldData)=>{
+          return [...oldData,...data]
+        })
+      } catch (error) {
+        console.log(error)
+      }
+      setIsLoading(false)
   }
+  
+  useEffect(()=>{
+      fetchImage()
+      // eslint-disable-next-line
+  },[page])
 
   useEffect(()=>{
-    fetchImage()
+      const event = window.addEventListener('scroll',()=>{
+          if(window.innerHeight+window.scrollY>document.body.offsetHeight-500 && !isLoading){
+              setPage((oldPage)=>{
+                return oldPage+1
+              })
+          }
+      })
+      return ()=>window.removeEventListener('scroll',event)
+      // eslint-disable-next-line
   },[])
+
   return (
     <main>
-      <h1>Infinite Scroll Photo | Unsplash API</h1>
-      <section className='photos'>
-        <div className="display-photo"> 
-          {photos.map((data,index)=>{
-            return <PhotoComponent key={index} {...data}/>
-          })}
-        </div>
-      </section>
+        <h1>Infinite Scroll Photo | Unsplash API</h1>
+        <section className="photos">
+            <div className="display-photo">
+                {photos.map((data,index)=>{
+                    return <PhotoComponent key={index} {...data}/>
+                })}
+            </div>
+        </section>
     </main>
   );
 }
